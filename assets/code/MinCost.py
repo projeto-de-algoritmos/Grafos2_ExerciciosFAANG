404500fd54d22fb08c2ca8a142ebb2820f5ac49f
Gabriel <gabrielb.alencarr@gmail.com>
2023-10-16 23:01:27 -0300

    Adicionando exec1

---
+++ assets/code/MinCost.py
@@ -0,0 +1,31 @@
+import heapq
+from typing import List
+
+class Solution(object):
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        N = len(points) 
+        adj = {i:[] for i in range(N)} # para cada i, tem vizinho lista de [cost, node]
+
+        #arestas
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i + 1, N):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        #prim
+        totalCost = 0
+        visited = set()
+        minHeap = [(0, 0)] # (cost, node)
+
+        while len(visited) < N:
+            cost, i = heapq.heappop(minHeap)
+            if i in visited:
+                continue
+            totalCost += cost
+            visited.add(i)
+            for vizcost, viz in adj[i]:
+                heapq.heappush(minHeap, [vizcost, viz])
+        return totalCost
